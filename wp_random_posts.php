9fa7dc08e2fc87a6c278097b6613af7f4dc2c98f
xninedesign <45494833+xninedesign@users.noreply.github.com>
2024-07-06 12:01:23 +0200

    Update wp_random_posts.php

--- wp_random_posts.php
+++ wp_random_posts.php
@@ -15,82 +15,82 @@
 *
 */
 
-  function random_postsids( $post_type = "post" , $termid, $multiplier = 2, $numberids, $issticked = false ) {
+function random_postsids( $post_type = "post" , $termid, $multiplier = 2, $numberids, $issticked = false ) {
 
-		if ( !$post_type  ) { return array(); exit; }
-		if ( term_exists( (int)$termid ) == null ) $termid = '';
+	if ( !$post_type  ) { return array(); exit; }
+	if ( term_exists( (int)$termid ) == null ) $termid = '';
 
-		if ( $termid == 0 || $termid == "" ) {
+	if ( $termid == 0 || $termid == "" ) {
+		
+		$cat = '';
+		$topicq = '';
+		$sticky = '';
+		
+	} else {
+		
+		if ( $post_type == "post" ) {
 			
-			$cat = '';
+			$cat = $termid;
 			$topicq = '';
-			$sticky = '';
+			
+			if ( $issticked == true ) $sticky = get_option( 'sticky_posts' );
+			else 
+				$sticky = array();				
 			
 		} else {
 			
-			if ( $post_type == "post" ) {
-				
-				$cat = $termid;
-				$topicq = '';
-				
-				if ( $issticked == true ) $sticky = get_option( 'sticky_posts' );
-				else 
-					$sticky = array();				
-				
-			} else {
-				
-				$cat = '';
-				$topicq = array ( array(
-							'taxonomy' => 'bsnewscat',
-							'field' => 'term_taxonomy_id',
-							'terms' => $termid,
-							)
-						);
-				$sticky = array();
-				
-			}
-		
+			$cat = '';
+			$topicq = array ( array(
+						'taxonomy' => 'bsnewscat',
+						'field' => 'term_taxonomy_id',
+						'terms' => $termid,
+						)
+					);
+			$sticky = array();
+			
 		}
-		
-		$args = array(
-			'post_type' 		=> $post_type,
-			'post_status' 		=> 'publish',
-			'numberposts' 		=> $numberids * $multiplier,
-			'tax_query' 			=> $topicq,
-			'cat' 					=> $cat,
-			'fields' 			=> 'ids',
-			'post__in' 			=> $sticky,
-			'no_found_rows' 	=> true
-		);
-		
-		$posts = get_posts( $args );
-
-		$ids = [];
-		$i=0;
-		foreach ( $posts as $post ) : 
-			
-			$ids[] = $post;
-			
-		$i++;
-		endforeach;
-		
-		$countids = count($ids);
-		if ( $countids < ( $numberids * $multiplier ) ) { 
-			$numberids = $countids;
-		}
-		
-		$keys = array_rand($ids,$numberids) ;
-		if ( $keys !== 0 ) {
-			
-			$post_in = [];
-			foreach ($keys as $key => $k) :
-			$post_in[] = $ids[$k];
-			endforeach;
-		
-			return $post_in;
-		
-		} else return $ids;
-
+	
 	}
+	
+	$args = array(
+		'post_type' 		=> $post_type,
+		'post_status' 		=> 'publish',
+		'numberposts' 		=> $numberids * $multiplier,
+		'tax_query' 			=> $topicq,
+		'cat' 					=> $cat,
+		'fields' 			=> 'ids',
+		'post__in' 			=> $sticky,
+		'no_found_rows' 	=> true
+	);
+	
+	$posts = get_posts( $args );
+
+	$ids = [];
+	$i=0;
+	foreach ( $posts as $post ) : 
+		
+		$ids[] = $post;
+		
+	$i++;
+	endforeach;
+	
+	$countids = count($ids);
+	if ( $countids < ( $numberids * $multiplier ) ) { 
+		$numberids = $countids;
+	}
+	
+	$keys = array_rand($ids,$numberids) ;
+	if ( $keys !== 0 ) {
+		
+		$post_in = [];
+		foreach ($keys as $key => $k) :
+		$post_in[] = $ids[$k];
+		endforeach;
+	
+		return $post_in;
+	
+	} else return $ids;
+
+}
 
 ?>
